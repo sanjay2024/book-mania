9d9818e952800b6c74d1b8a35f1b534b23bea643
sanjay <20euit128@skcet.ac.in>
2023-06-05 11:22:56 +0530

    Home.css modified

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import videoSource from '../../assets/img/books.mp4'
+import profileImg from '../../assets/img/book.jpg'
 const Home = () => {
   return (
-    <div className='Container'>
-      <div className='Content'>
-        <div className='SubContent'>
+    <div className="Container">
+      <video autoPlay="autoplay" loop="loop" muted className="Video">
+        <source src={videoSource} type="video/mp4" />
+      </video>
+      <div className="Content">
+        <div className="SubContent">
           <h1>Book Catolog</h1>
           <p>Manage your Books with Ease</p>
-          <button type='button' className='btn btn-outline-dark'>
-            <Link to='/login'>Get started</Link>
+          <button type="button" className="btn btn-outline-dark">
+            <Link className="text-link " to="/register">
+              Get started
+            </Link>
           </button>
+          <img src={profileImg} alt="profile" />
         </div>
       </div>
     </div>
