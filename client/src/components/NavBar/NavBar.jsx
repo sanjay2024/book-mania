5e2f02db2e7a6dce42ec76881c8427d67ef28f5c
sanjay <20euit128@skcet.ac.in>
2023-06-05 14:27:26 +0530

    deleted unwanted varaibles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate,useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../../redux/action/user/userActions";
 
