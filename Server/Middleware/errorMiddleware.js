cade2c2850f5f167533d74e7545b319027de94ee
sanjay <20euit128@skcet.ac.in>
2023-06-05 16:19:41 +0530

    deleted errorAuthMiddleware

--- Server/Middleware/errorMiddleware.js
+++ Server/Middleware/errorMiddleware.js
@@ -1,10 +1,3 @@
-
-const notfoundErrorMiddleware = (req, res, next) => {
-  const error = new Error(`Not found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
-
 const errorMiddlewareHandler = (err, req, res, next) => {
   //Set your status code
   const errorStatusCode = res.statusCode === 200 ? 500 : res.statusCode;
@@ -15,4 +8,4 @@ const errorMiddlewareHandler = (err, req, res, next) => {
   });
 };
 
-module.exports = { errorMiddlewareHandler, notfoundErrorMiddleware };
+module.exports = { errorMiddlewareHandler};
